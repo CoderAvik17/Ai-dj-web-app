81b0e10b68ff5a152f4346e03a2e5fbe83db4611
CoderAvik17 <79036320+CoderAvik17@users.noreply.github.com>
2022-08-05 19:45:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song="";
 song1="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -14,6 +18,9 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.PoseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
@@ -24,4 +31,21 @@ function draw()
 function play()
 {
     song.play();
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    console.log(results);
+    leftWristX = results[0].pose.leftWristX.x;
+    leftWristY = results[0].pose.leftWristY.y;
+    console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWristX.x;
+    rightWristY = results[0].pose.rightWristY.y;
+    console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
 }
